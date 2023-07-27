6448a3fb04235fc1bb242504f3241bd53c431077
Denis Popkov <pkvdenis@gmail.com>
2023-07-27 21:36:15 +0300

    mjs: 08 - shopping list: remove items, clear ui state

--- m-js-f-t-b/08-shopping-list/script.js
+++ m-js-f-t-b/08-shopping-list/script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const filter = document.getElementById('filter');
+const clearButton = document.getElementById('btn-clear');
 
 // ---------------
 // Event Listeners
@@ -16,6 +18,7 @@ function addItem(e) {
   }
   itemList.insertAdjacentElement('beforeend', createItemListItem(itemName));
   itemInput.value = '';
+  changeStylesForUiState();
 }
 
 function createItemListItem(itemName) {
@@ -31,4 +34,52 @@ function createRemoveBtn() {
   removeBtn.textContent = 'x';
   return removeBtn;
 }
+
+function removeItem(e) {
+  if (e.target.tagName === 'SPAN' && e.target.textContent === 'x') {
+    const li =
+      e.target.parentElement.tagName === 'LI' ? e.target.parentElement : null;
+    if (li !== null && li.parentElement.classList.contains('items')) {
+      if (
+        confirm(
+          `Are you sure to remove "${li.firstChild.textContent.trim()}" item?`
+        )
+      ) {
+        li.remove();
+        checkUI();
+      }
+    }
+  }
+}
+
+function checkUI() {
+  const listIsEmpty = document.querySelectorAll('#item-list > li').length === 0;
+  changeStylesForUiState(listIsEmpty);
+}
+
+function changeStylesForUiState(listIsEmpty = false) {
+  if (listIsEmpty) {
+    filter.style.display = 'none';
+    clearButton.style.display = 'none';
+  } else {
+    filter.style.display = 'block';
+    clearButton.style.display = 'block';
+  }
+}
+
+function clearAllItems(e) {
+  const ul = document.querySelector('ul#item-list ');
+  // The simplest, but not the best way to remove all items:
+  //   ul.innerHTML = '';
+
+  // The preferred way:
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  changeStylesForUiState(true);
+}
+
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearAllItems);
+checkUI();
