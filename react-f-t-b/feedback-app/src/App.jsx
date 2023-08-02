54e39757407a2a04c6d6a92b3edcb529baa633fd
Denis Popkov <pkvdenis@gmail.com>
2023-08-02 23:05:12 +0300

    react-f-t-b: feedback-app: dynamic values and lists in JSX

--- react-f-t-b/feedback-app/src/App.jsx
+++ react-f-t-b/feedback-app/src/App.jsx
@@ -1,10 +1,28 @@
 // import './App.css'
 
 function App() {
+  const title = 'Blog Post';
+  const body = 'This is blog post about Dynamic Values and Lists in JSX';
+  const comments = [
+    { id: 1, text: 'Comment 1' },
+    { id: 2, text: 'Comment 2' },
+    { id: 3, text: 'Comment 3' },
+  ];
+
   return (
-    <>
-      <h1>Hello from the app Component</h1>
-    </>
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            // Each child in a list should have a unique "key" prop:
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
