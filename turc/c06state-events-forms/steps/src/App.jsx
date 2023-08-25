d8efc65807d5c00f8a629ecda39ff462ae2e21c9
Denis Popkov <pkvdenis@gmail.com>
2023-08-25 23:35:14 +0300

    turc: c06state-events-forms: project steps

---
+++ turc/c06state-events-forms/steps/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import './App.css';
+
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep((v) => v - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < messages.length) {
+      setStep((v) => v + 1);
+    }
+  }
+
+  function toggleClose() {
+    setIsOpen((v) => !v);
+  }
+
+  return (
+    <div className="container">
+      <button className="close" onClick={toggleClose}>
+        {isOpen ? 'Close' : 'Open'}
+      </button>
+      {isOpen && (
+        <div className="app">
+          <Steps step={step} />
+          <Message step={step} />
+          <Buttons handlePrevious={handlePrevious} handleNext={handleNext} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Steps({ step }) {
+  return (
+    <div className="steps">
+      {messages.map((_, index) => (
+        <Step stepNumber={index + 1} isActive={step > index} key={index} />
+      ))}
+    </div>
+  );
+}
+
+function Step({ stepNumber, isActive }) {
+  return <div className={isActive ? 'step active' : 'step'}>{stepNumber}</div>;
+}
+
+function Message({ step }) {
+  return (
+    <h3>
+      Step {step}: {messages[step - 1]}
+    </h3>
+  );
+}
+
+function Buttons({ handlePrevious, handleNext }) {
+  return (
+    <div className="buttons">
+      <button onClick={handlePrevious}>Previous</button>
+      <button onClick={handleNext}>Next</button>
+    </div>
+  );
+}
